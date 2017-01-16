24295fae27c17129baeed955ab9481d244fef79e
pivovard <pivovar.david@gmail.com>
2017-01-16 23:49:21 +0100

    erase switches and PlayerLists based on number of players in game

--- GameManager.cpp
+++ GameManager.cpp
@@ -6,50 +6,14 @@
 
 int GameManager::game_count = 0;
 static int player_count[3] = {0, 0, 0};
-int GameManager::player_count2 = 0;
-int GameManager::player_count3 = 0;
-int GameManager::player_count4 = 0;
 
 vector<Game*> GameManager::GameList;
 static vector<Player*> PlayerList[3] = {vector<Player*>(), vector<Player*>(), vector<Player*>()};
-vector<Player*> GameManager::PlayerList2;
-vector<Player*> GameManager::PlayerList3;
-vector<Player*> GameManager::PlayerList4;
 
 
 Player* GameManager::PlayerConnect(string nick, char *ip, int socket, int n)
 {
     Player *pl;
-    /*switch(n)
-    {
-        case 2:
-            pl = new Player(nick, ip, socket, player_count2);
-            PlayerList2.push_back(pl);
-            player_count2++;
-
-            if((PlayerList2.size() % 2) == 0){
-                GameManager::Start2Game();
-            }
-            break;
-        case 3:
-            pl = new Player(nick, ip, socket, player_count3);
-            PlayerList3.push_back(pl);
-            player_count3++;
-
-            if((PlayerList3.size() % 3) == 0){
-                GameManager::Start3Game();
-            }
-            break;
-        case 4:
-            pl = new Player(nick, ip, socket, player_count4);
-            PlayerList4.push_back(pl);
-            player_count4++;
-
-            if((PlayerList4.size() % 4) == 0){
-                GameManager::Start4Game();
-            }
-            break;
-    }*/
 
     pl = new Player(nick, n, ip, socket, PlayerList[n-2].size());
     PlayerList[n-2].push_back(pl);
@@ -80,31 +44,6 @@ void GameManager::ResolveTurn(string msg)
 
 Player* GameManager::GetPlayer(string nick, int n)
 {
-    /*switch(n)
-    {
-        case 2:
-            for(int i = 0; i < PlayerList2.size(); i++){
-                if(nick == PlayerList2[i]->nick && PlayerList2[i]->connected < 2){
-                    return PlayerList2[i];
-                }
-            }
-            break;
-        case 3:
-            for(int i = 0; i < PlayerList3.size(); i++){
-                if(nick == PlayerList3[i]->nick && PlayerList2[i]->connected < 2) {
-                    return PlayerList3[i];
-                }
-            }
-            break;
-        case 4:
-            for(int i = 0; i < PlayerList4.size(); i++){
-                if(nick == PlayerList4[i]->nick && PlayerList2[i]->connected < 2) {
-                    return PlayerList4[i];
-                }
-            }
-            break;
-    }*/
-
     for(int i = 0; i < PlayerList[n-2].size(); i++){
         if(nick == PlayerList[n-2][i]->nick && PlayerList[n-2][i]->connected < 2){
             return PlayerList[n-2][i];
@@ -116,37 +55,6 @@ Player* GameManager::GetPlayer(string nick, int n)
 
 int GameManager::CheckNick(string nick, int n)
 {
-    /*switch(n)
-    {
-        case 2:
-            for(int i = 0; i < PlayerList2.size(); i++){
-                if(nick == PlayerList2[i]->nick){
-                    if(PlayerList2[i]->connected == 0) return 2; //pripojen hrajici, nick neni k dispozici
-                    if(PlayerList2[i]->connected == 1) return 1; //znovu pripojeni do hry
-                    break;
-                }
-            }
-            break;
-        case 3:
-            for(int i = 0; i < PlayerList3.size(); i++){
-                if(nick == PlayerList3[i]->nick) {
-                    if(PlayerList3[i]->connected == 0) return 2; //pripojen hrajici, nick neni k dispozici
-                    if(PlayerList3[i]->connected == 1) return 1; //znovu pripojeni do hry
-                    break;
-                }
-            }
-            break;
-        case 4:
-            for(int i = 0; i < PlayerList4.size(); i++){
-                if(nick == PlayerList4[i]->nick) {
-                    if(PlayerList4[i]->connected == 0) return 2; //pripojen hrajici, nick neni k dispozici
-                    if(PlayerList4[i]->connected == 1) return 1; //znovu pripojeni do hry
-                    break;
-                }
-            }
-            break;
-    }*/
-
     for(int i = 0; i < PlayerList[n-2].size(); i++){
         if(nick == PlayerList[n-2][i]->nick) {
             if(PlayerList[n-2][i]->connected == 0) return 2; //pripojen hrajici, nick neni k dispozici
--- GameManager.h
+++ GameManager.h
@@ -29,20 +29,11 @@ public:
 private:
     static int game_count;
     //static int player_count[3];
-    static int player_count2;
-    static int player_count3;
-    static int player_count4;
 
     static vector<Game*> GameList;
     //static vector<Player*> PlayerList[3];
-    static vector<Player*> PlayerList2;
-    static vector<Player*> PlayerList3;
-    static vector<Player*> PlayerList4;
 
     static void StartGame(int n);
-    static void Start2Game();
-    static void Start3Game();
-    static void Start4Game();
 
     static void Remove(Player *pl);
     static void DestroyGame(Game *g);
